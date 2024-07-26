8041e03df9dccdd42f024390f931a2e36874b8d2
shivapabbu1 <shivapabbu718@gmail.com>
2024-07-26 11:30:04 +0530

    template ref

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <h1>AppComponent</h1>
-    <button @click="showParentComponent = !showParentComponent">
-      Toggle Parent Component
-    </button>
-    <ParentComponent v-if="showParentComponent"/>
-
-    <div>
-      <ProfileList/>
-    </div>
+    <TempParent />
   </div>
 </template>
 
 <script>
-import ParentComponent from './components/ParentComponent.vue';
-import ProfileList from './components/ProfileList.vue';
+import TempParent from './components/TempParent.vue';
+
 export default {
   name: 'App',
   components: {
-    ParentComponent,
-    ProfileList
-  },
-  data() {
-    return {
-      showParentComponent: true
-    };
+    TempParent
   }
-};
+}
 </script>
 
 <style>
@@ -37,17 +23,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-button {
-  margin: 10px;
-  padding: 10px;
-  background-color: #42b983;
-  color: white;
-  border: none;
-  cursor: pointer;
-  border-radius: 5px;
-}
-button:hover {
-  background-color: #2c9e6a;
 }
 </style>
---
+++ src/components/TempChild.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <p>Child Component</p>
+      <button @click="childMethod">Click me</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TempChild',
+    methods: {
+      childMethod() {
+        alert('Child component method called!');
+      }
+    }
+  }
+  </script>
+  
