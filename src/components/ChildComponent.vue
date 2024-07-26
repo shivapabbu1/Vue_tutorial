78d63ffd3a5fb30251c1fc3e244f7a840c727929
shivapabbu1 <shivapabbu718@gmail.com>
2024-07-26 17:08:05 +0530

    update

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,37 +1,42 @@
 <template>
-    <div>
-      <h2>Child</h2>
-      <SubChild />
-    </div>
-  </template>
-  
-  <script>
-  import SubChild from './SubChild.vue';
-  
-  export default {
-    name: 'ChldCompnent',
-    components: {
-      SubChild
-    },
-    created() {
-      console.log('Child component created');
-    },
-    mounted() {
-      console.log('Child component mounted');
-    },
-    updated() {
-      console.log('Child component updated');
-    },
-    beforeUnmount() {
-      console.log('Child component beforeUnmount');
-    },
-    unmounted() {
-      console.log('Child component destroyed');
-    }
-  };
-  </script>
-  
-  <style scoped>
-  /* Add component-specific styles here */
-  </style>
-  
+  <div>
+    <h2>Child</h2>
+    <button @click="showsubChild = !showsubChild">Toggle SubChild</button>
+    <SubChild v-if="showsubChild" />
+  </div>
+</template>
+
+<script>
+import SubChild from './SubChild.vue';
+
+export default {
+  name: 'ChildComponent',
+  data() {
+    return {
+      showsubChild: true
+    };
+  },
+  components: {
+    SubChild
+  },
+  created() {
+    console.log('Child component created');
+  },
+  mounted() {
+    console.log('Child component mounted');
+  },
+  updated() {
+    console.log('Child component updated');
+  },
+  beforeUnmount() {
+    console.log('Child component beforeUnmount');
+  },
+  unmounted() {
+    console.log('Child component destroyed');
+  }
+};
+</script>
+
+<style scoped>
+/* Add component-specific styles here */
+</style>
