fc66b31aa321ed6b2e31ad627b5c3336d4931313
shivapabbu1 <shivapabbu718@gmail.com>
2024-07-26 17:08:56 +0530

    update

---
+++ src/components/CounterComp.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+<button @click="increment">CLick {{ count }} times</button>
+    </div>
+</template>
+
+<script>
+import CounerMixin from "./MIxins/Counter"
+    export default {
+        name: 'CounterComp',
+        mixins: [CounerMixin],
+        
+        data(){
+        return{
+            count: 0
+        }
+    },
+    methods: {
+        increment(){
+             this.count +=1;
+        },
+        
+    },
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
