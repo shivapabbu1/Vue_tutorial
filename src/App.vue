e848ce97f31bc5ad0f40bd1159cdc181336f2479
shivapabbu1 <shivapabbu718@gmail.com>
2024-07-26 14:36:08 +0530

    TOrefs

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,22 @@
 <template>
-  <div id="app">
-  
-    <h1>hello form options {{ name1 }}</h1>
-    <h2>hello from compostions with name {{ fname }} {{ lname }} as {{ role }}</h2>
-    <div>
-<h1>{{ qusn }}</h1>
-    <p>{{ ans }}</p>
-    </div>
-
-    <div>
-      <h2>{{ greetfromreactive }}</h2>
-      <p>{{ ansformreactive }}</p>
-    </div>
-
-    </div>
+  <div>
+<DemoONe/>
+<DemoTwo/>
+  </div>
 </template>
 
 <script>
-
-import { reactive, ref } from 'vue';
-
-export default {
-  name: 'App',
-  setup() {
-
-
-    const state=reactive({
-    fname:'kiran',
-    lname:'kommu',
-    role:'Fullstack developer'
-   })
-      
-    // Create a reactive reference for the count
-    const fname = ref("shivaprasad");
-    const lname=ref("pabbu")
-    const role=ref("developer")
-   
-    const qusn= `Hello ${fname.value}  ${lname.value}.What are you?`
-    const ans=`hey.. i am ${role.value}`
-
-    const greetfromreactive=`Hello ${state.fname}  ${state.lname}.What are you?`
-    const ansformreactive=`hey.. i am ${state.role}`
- 
-  
-    // Return the refs and methods to the template
-    return {
-      fname,
-      lname,
-      role,qusn,ans
-      ,
-      greetfromreactive,
-      ansformreactive
-      
-    
-    };
-  },
-  data(){
-    return {
-      name1: 'shivapabbu'
+import DemoONe from './components/DemoONe.vue';
+import DemoTwo from './components/DemoTwo.vue';
+  export default {
+    name :'App',
+    components:{
+      DemoONe,
+      DemoTwo
     }
   }
-};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
-``
